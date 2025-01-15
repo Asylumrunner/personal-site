807b47c8136e4fa454f20360ec626133da8cb5dd
Michael Burdick <asylumrunner@gmail.com>
2025-01-14 16:16:23 -0800

    break down about me section

--- src/components/BodyBlock.tsx
+++ src/components/BodyBlock.tsx
@@ -1,8 +1,13 @@
 function BodyBlock({section}) {
+    
+    const text = section.text.map((block: string) => {
+        return (<div>{block}<br/><br/></div>)
+    })
+
     return (
         <div>
             <p className="font-oswald text-2xl mb-4">{section.header}</p>
-            <p>{section.text}</p>
+            <p>{text}</p>
             <br/>
         </div>
     )
